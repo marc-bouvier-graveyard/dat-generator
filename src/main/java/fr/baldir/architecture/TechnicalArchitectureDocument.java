7eb7be06277779ede43783fbd82577a249b974f1
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-08-10 21:38:53 +0200

    using qute for templating

--- src/main/java/fr/baldir/architecture/TechnicalArchitectureDocument.java
+++ src/main/java/fr/baldir/architecture/TechnicalArchitectureDocument.java
@@ -1,5 +1,6 @@
 package fr.baldir.architecture;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,12 +10,20 @@ import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
+import io.quarkus.qute.Template;
+
 @Path("/technical-architecture-document")
 public class TechnicalArchitectureDocument {
 
+    @Inject
+    Template header;
+
+    @Inject
+    Template intro;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String fullDemoTemplate() {
         return "hello";
     }
 
@@ -24,10 +33,8 @@ public class TechnicalArchitectureDocument {
     public String generate(@MultipartForm GenerateDocument command) {
 
         StringBuilder builder = new StringBuilder();
-        builder.append("# Document technique d'architecture");
-        builder.append('\n');
-        builder.append('\n');
-        builder.append(command.outputType);
+        builder.append(header.data("test", command.outputType).render());
+        builder.append(intro.data("test", command.outputType).render());
         return builder.toString();
     }
 
