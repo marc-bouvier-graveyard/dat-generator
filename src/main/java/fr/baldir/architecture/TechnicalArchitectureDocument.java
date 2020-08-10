1084fc73b42c38ace1facb54b5977bec8fbe8b59
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-08-10 21:13:05 +0200

    basic generation with multipart form

--- src/main/java/fr/baldir/architecture/TechnicalArchitectureDocument.java
+++ src/main/java/fr/baldir/architecture/TechnicalArchitectureDocument.java
@@ -1,9 +1,13 @@
 package fr.baldir.architecture;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 @Path("/technical-architecture-document")
 public class TechnicalArchitectureDocument {
@@ -14,5 +18,17 @@ public class TechnicalArchitectureDocument {
         return "hello";
     }
 
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public String generate(@MultipartForm GenerateDocument command) {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("# Document technique d'architecture");
+        builder.append('\n');
+        builder.append('\n');
+        builder.append(command.outputType);
+        return builder.toString();
+    }
 
 }
